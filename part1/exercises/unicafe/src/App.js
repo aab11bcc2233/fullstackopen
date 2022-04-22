7b3100736d174665dfba623771f056d4416aa501
hetianpeng <hetianpeng@dongyin.net>
2022-04-22 16:19:23 +0800

    part1 Exercises1.7 unicafe step2

--- part1/exercises/unicafe/src/App.js
+++ part1/exercises/unicafe/src/App.js
@@ -13,10 +13,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  console.log('good counter', good)
-  console.log('neutral counter', neutral)
-  console.log('bad counter', bad)
-
   const clickGood = () => {
     setGood(good + 1)
   }
@@ -26,6 +22,33 @@ const App = () => {
   const clickBad = () => {
     setBad(bad + 1)
   }
+
+  const calcAll = () => good + neutral + bad
+
+  const calcAverage = () => {
+    const all = calcAll()
+    if (all === 0) {
+      return 0
+    }
+
+    return (good * 1 + neutral * 0 + bad * -1) / all
+  }
+
+  const calcPostivie = () => {
+    const all = calcAll()
+    if (all === 0) {
+      return 0
+    }
+   return good / all * 100.0
+  }
+
+
+  console.log('good counter', good)
+  console.log('neutral counter', neutral)
+  console.log('bad counter', bad)
+  console.log('all', calcAll())
+  console.log('average', calcAverage())
+  console.log('positive', calcPostivie(), "%")
 
   return (
     <div>
@@ -37,6 +60,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {calcAll()}</div>
+      <div>average { calcAverage() }</div>
+      <div>positive { calcPostivie() } %</div>
     </div>
   )
 }
