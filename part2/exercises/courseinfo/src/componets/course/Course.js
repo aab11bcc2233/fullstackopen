370d416fa4fdda3d70cd021ef152fa79bfbd734a
hetianpeng <hetianpeng@dongyin.net>
2022-04-24 12:28:51 +0800

    part2 Exercises2.4-2.5 Course information

--- part2/exercises/courseinfo/src/componets/course/Course.js
+++ part2/exercises/courseinfo/src/componets/course/Course.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Header from "./header/Header"
 import Content from "./content/Content"
+import Total from "./total/Total"
 
 const Course = ({ course }) => {
     return (
         <div>
             <Header name={course.name} />
             <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     )
 }
