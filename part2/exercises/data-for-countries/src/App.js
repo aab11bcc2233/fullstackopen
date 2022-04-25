4fec93e1f9754e79f0c8025d55b556ceab736aaf
hetianpeng <hetianpeng@dongyin.net>
2022-04-25 16:47:10 +0800

    part2 2.14 Data for countries step3

--- part2/exercises/data-for-countries/src/App.js
+++ part2/exercises/data-for-countries/src/App.js
@@ -1,10 +1,50 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-const CountryItem = ({ index, country, clickShowCountry}) => {
+const CountryItem = ({ index, country, clickShowCountry }) => {
   return (
     <div>
       {country.name.common} <button onClick={() => clickShowCountry(index)}>show</button>
+    </div>
+  )
+}
+
+const Weather = ({ country }) => {
+
+  const [weatherData, setWeatherData] = useState()
+  const city = country.capital[0]
+
+  useEffect(
+    () => {
+      console.log('request weather')
+      const apiKey = process.env.REACT_APP_API_KEY_OPEN_WEATHER
+      // const [lat, lng] = country.capitalInfo.latlng
+      // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apiKey}`
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      axios.get(url)
+        .then((response) => {
+          const data = response.data
+          setWeatherData(data)
+        })
+    },
+    [city]
+  )
+
+  if (weatherData === undefined) {
+    return (<></>)
+  }
+
+  return (
+    <div>
+      <div>
+        <strong>temperature: </strong> {weatherData.main.temp - 273.15} °C {weatherData.weather[0].description}
+      </div>
+      <div>
+        <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt='weather icon' />
+      </div>
+      <div>
+        <strong>wind: </strong> {weatherData.wind.speed} m/s
+      </div>
     </div>
   )
 }
@@ -23,6 +63,9 @@ const Country = ({ country }) => {
         </ul>
       </div>
       <img src={country.flags.png} alt='flag' />
+
+      <h2>Weather in {country.capital[0]}</h2>
+      <Weather country={country} />
     </div>
   )
 }
@@ -80,7 +123,7 @@ function App() {
     const text = event.target.value
     setSearchText(text)
     setShowIndex(-1)
-    
+
     if (text === '') {
       setSearchResults([])
     } else {
@@ -100,8 +143,8 @@ function App() {
         find countries <input value={searchText} onChange={onInputSearchText} />
       </div>
 
-      { showIndex > -1 ? <Country country={searchResults[showIndex]} /> : <SearchResult countries={searchResults} clickShowCountry={clickShowCountry} />}
-      
+      {showIndex > -1 ? <Country country={searchResults[showIndex]} /> : <SearchResult countries={searchResults} clickShowCountry={clickShowCountry} />}
+
     </div>
   );
 }
